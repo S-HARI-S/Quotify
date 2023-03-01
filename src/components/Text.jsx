6b06afae7092df01243ee07f0f321f92e003de6b
S-HARI-S <sreeharisureshmltr@gmail.com>
2023-03-01 23:48:41 +0530

    better ux

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -128,6 +128,8 @@ export default function text() {
             placeholder="Describe how background image of qoute should looks like"
             className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
+          <p className="text-white">Note : the default color of texts are black so specify some light colors for background for better visibility.</p>
+
         </div>
         <div className="flex justify-center">
           <button
